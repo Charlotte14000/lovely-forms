92dba01732335595ba796674b335bd1a345ed892
Charlotte14 <vaudourc@yahoo.fr>
2018-10-30 18:10:49 +0100

    final commit and pushing to Heroku

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,11 @@
 class UserController < ApplicationController
   def new
+    @user=User.new
   end
 
   def create
+    @user = User.new('username' => params[:username],
+                  'email' => params[:email],
+                  'bio' => params[:bio])
   end
 end
